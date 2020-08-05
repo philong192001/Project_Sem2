0914b049a2428ea9e4027f6288ab6879f5890f51
Nguyen Minh Nghia (Aptech HN) <nghianmth1907030@fpt.edu.vn>
2020-08-05 14:45:16 +0700

    updateroute 5/8

--- routes/pageCoffe.php
+++ routes/pageCoffe.php
@@ -5,20 +5,32 @@ use App\Category;
 
 
     Route::group(['prefix' => '/pageCoffe'], function() {
-        
-        Route::get('home', 'Coffe\PageController@showHome')->name('home_page');
-        
+//home      
+        Route::get('home', 'Coffe\PageController@showHome')->name('show-home');
+                
 
-        Route::get('danh-muc-san-pham/{id}',[
-             'as' => 'productCategory',
-             'uses' => 'Coffe\CategoryController@showCategoryProduct'
-        ]);
+       
+//MENU
+        Route::get('menu', 'Coffe\MenuController@showMenu')->name('show-menu');
 
+//Services
+        Route::get('services', 'Coffe\ServicesController@showServices')->name('show-services');
 
-        Route::get('chi-tiet-san-pham/{id}',[
-        	'as'=>'productDetail',
-        	'uses'=>'Coffe\CategoryController@showDetailProduct'
-        ]);        
+//Blog
+        Route::get('blog', 'Coffe\BlogController@showBlog')->name('show-blog');
+
+//about
+        Route::get('about', 'Coffe\AboutController@showAbout')->name('show-about');
+//shop
+        Route::get('shop', 'Coffe\ShopController@showShop')->name('show-shop');
+//productDetail
+        Route::get('productDetail', 'Coffe\ProductController@ShowProduct')->name('show-productDetail');
+//contact
+        Route::get('contact', 'Coffe\ContactController@showContact')->name('show-contact');
+//cart
+        Route::get('cart', 'Coffe\CartController@showCart')->name('show-cart');
+//checkout
+        Route::get('checkout', 'Coffe\CheckoutController@showCheckout')->name('show-checkout');
 
     });
  ?> 
